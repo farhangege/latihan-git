e190a0cf9aaae04a2f7b6ac6dd4dfcce39c89399
farhangege <101858946+farhangege@users.noreply.github.com>
2022-04-30 00:44:53 +0700

    Add files via upload

---
+++ Biodata.php
@@ -0,0 +1,21 @@
+<?php
+class Biodata extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>PERKENALKAN </h1>";
+        echo " <p> BIODATA DIRI SAYA</p>";
+        echo " <p> NAMA                : Farhan Eka Saputra</p>";
+        echo " NIM                     : 17190067 ";
+        echo " <p> KELAS               : 17.6A.25 </p>";
+        echo " Fakultas                : Teknologi INformasi";
+        echo " <p> UNIVERSITAS         : BINA SARANA INFORMATIKA</p>";
+        echo " JENIS KELAMIN           : LAKI-LAKI";
+        echo " <p>TEMPAT,TANGGAL LAHIR : JAKARTA, 21 NOVEMBER 2000</p>";
+        echo " GOLONGAN DARAH          : O";
+        echo " <p>ALAMAT               : Jalan Bojong Raya Rt08/04 </p>";
+        echo " HOBBY                   : Swimming ";
+        echo " <p> E-MAIL              : farhanajah211@gmail.com</p>";
+        echo " NO.HP                   : 081383572181";
+    }
+}
