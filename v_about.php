53d1e24760120d2d3f36f4f7fdc18d8bef06866f
farhangege <101858946+farhangege@users.noreply.github.com>
2022-04-30 00:31:08 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,23 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Farhan</li>
+ <li>Nama Belakang : Eka Saputra</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Bojong Raya/8 </li>
+ </ul>
+
+ <h4>Tempat Lahir, Tanggal Lahir</h4>
+ <ul type="none">
+ <li>Jakarta, 21 November 2000</li>
+ </ul>
+ <h4>MY HOBBY</h4>
+ <ul type="square">
+ <li>Swimming </li>
+ <li>Tennis</li>
+ </ul>
+</section>
